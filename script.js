1344fd9d17f9257e8e368f6ce92e555034cda3c6
ryanc7064 <116000814+ryanc7064@users.noreply.github.com>
2022-10-17 14:36:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
+let ownerName = "Ryan C"; // update the part BETWEEN the "quotes"
 let userName = "YourUsername"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
